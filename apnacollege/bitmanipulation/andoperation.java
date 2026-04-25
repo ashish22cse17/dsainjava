fb2d2aa15bc657ae31733cd9ccaa89ee0ca25377
Ashish <ashishcpr697@gmail.com>
2026-04-25 23:52:13 +0530

    bit manipulation start

---
+++ apnacollege/bitmanipulation/andoperation.java
@@ -0,0 +1,14 @@
+package bitmanipulation;
+
+public class andoperation {
+    
+    public static void main(String[] args) {
+        int a =5,b=6;
+        System.out.println(a|b);
+        System.out.println(a&b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(5<<2);//  a<<b=a*2^b
+        System.out.println(5>>2);//  a<<b=a/2^b
+    }
+}
---
+++ apnacollege/bitmanipulation/evenodd.java
@@ -0,0 +1,13 @@
+package bitmanipulation;
+
+public class evenodd {
+    public static void main(String[] args) {
+        int n =5;
+        int check =1;
+        if((n&check)==1){
+            System.out.println("yes odd");
+        }
+        else
+            System.out.println("yes even");
+}
+}
